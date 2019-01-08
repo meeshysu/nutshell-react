626282f3161bf10a107eec1e33cc911d053e47a7
Michelle Beshears <meeshysu@gmail.com>
2019-01-07 20:49:36 -0600

    logging in and out work now

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from '../components/Auth/Auth';
+import authRequests from '../helpers/data/authRequests';
+import connection from '../helpers/data/connection';
 import './App.scss';
+import MyNavbar from '../components/MyNavbar/myNavbar';
+
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  };
+
   render() {
+    const { authed } = this.state;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      this.setState({ authed: false });
+    };
+
+    if (!authed) {
+      return (
+        <div className="App">
+          <MyNavbar isAuthed={authed} logoutClickEvent={logoutClickEvent}/>
+          <Auth isAuthenticated={this.isAuthenticated} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        <Auth />
+        <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
       </div>
     );
   }
