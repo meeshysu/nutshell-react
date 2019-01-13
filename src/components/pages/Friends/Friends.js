d56697627a45b67bb69ca13c1967d5cd916427aa
Michelle Beshears <meeshysu@gmail.com>
2019-01-13 13:55:34 -0600

    users are console logging

--- src/components/pages/Friends/Friends.js
+++ src/components/pages/Friends/Friends.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import smashRequests from '../../../helpers/data/smashRequests';
+import authRequests from '../../../helpers/data/authRequests';
 import './Friends.scss';
 
 class Friends extends React.Component {
+  state = {
+    users: [],
+    uid: '',
+  }
+
+  componentDidMount() {
+    smashRequests
+      .usersAndFriends(authRequests.getCurrentUid())
+      .then((users) => {
+        console.log(users);
+      })
+      .catch(error => console.log('stuff broke', error));
+  }
+
   render() {
     return (
       <div className='Friends'>
-      <h2>Muh Friends</h2>
+        <h2>Muh Friends</h2>
       </div>
     );
   }
