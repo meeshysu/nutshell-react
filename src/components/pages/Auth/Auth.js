4c5229668a0b6c19fd670d9e5f8cbd42c8498a2f
Michelle Beshears <meeshysu@gmail.com>
2019-01-13 15:01:06 -0600

    console logs my email and alternate email user uid

--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import authRequests from '../../../helpers/data/authRequests';
+import userRequests from '../../../helpers/data/userRequests';
 import './Auth.scss';
 
 import googleButton from './images/GoogleImage.png';
@@ -13,8 +14,20 @@ class Auth extends React.Component {
   authenticateUser = (e) => {
     e.preventDefault();
     authRequests.authenticate()
-      .then(() => {
-        this.props.history.push('/home');
+      .then((results) => {
+        console.log(results.user.uid);
+        userRequests.getUserByUid(results.user.uid)
+          .then((user) => {
+            if (!user) {
+              const userObject = {
+                uid: `${results.user.uid}`,
+                userName: `${results.user.displayName}`,
+                photo: `${results.user.photoURL}`,
+              };
+              userRequests.createUser(userObject);
+            }
+            this.props.history.push('/home');
+          });
       })
       .catch(err => console.error('there is an issue with auth', err));
   }
@@ -23,7 +36,7 @@ class Auth extends React.Component {
     return (
       <div className="Auth">
         <button className="btn btn" onClick={this.authenticateUser}>
-          <img src={googleButton} alt="google login button" width="500px"/>
+          <img src={googleButton} alt="google login button" width="500px" />
         </button>
       </div>
     );
