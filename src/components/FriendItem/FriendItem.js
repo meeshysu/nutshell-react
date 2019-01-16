a8229b5ba55bd5df70c4f099296690129e34c690
Michelle Beshears <meeshysu@gmail.com>
2019-01-15 21:09:17 -0600

    makeNewFriend event created

--- src/components/FriendItem/FriendItem.js
+++ src/components/FriendItem/FriendItem.js
@@ -3,16 +3,31 @@ import { Button } from 'reactstrap';
 import PropTypes from 'prop-types';
 import friendShape from '../../helpers/propz/friendShape';
 import './FriendItem.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class FriendItem extends React.Component {
   static propTypes = {
     friends: friendShape,
     status: PropTypes.string,
     goodbyeFriend: PropTypes.func,
+    helloFriend: PropTypes.func,
   };
 
+  makeNewFriend = (e) => {
+    e.preventDefault();
+    const uid = authRequests.getCurrentUid();
+    const friendUid = e.target.id;
+    const { addFriend } = this.props;
+    const newFriend = {
+      uid,
+      friendUid,
+      isAccepted: true,
+      isPending: false,
+    };
+    addFriend(newFriend);
+  };
 
-  deleteFriendButton = (e) => {
+  deleteFriend = (e) => {
     e.preventDefault();
     const maybeMyFriendId = e.target.id;
     const { goodbyeFriend } = this.props;
@@ -24,14 +39,18 @@ class FriendItem extends React.Component {
     const makeButtons = () => {
       if (status === 'confirmed') {
         return (
-          <Button className='btn btn-danger' id={friend.friendRequestId} onClick={this.deleteFriendButton}>X</Button>
+          <Button className='btn btn-danger' id={friend.friendRequestId} onClick={this.deleteFriend}>X</Button>
         );
-      // } if (status === 'pending' && friend.friendRequest === 'them') {
-      //   return (
-      //     <div>
-      //       <Button className='btn btn-danger' id={friend.friendRequestId} onClick={this.deleteFriendButton}>X</Button>
-      //     </div>
-        // );
+        // } if (status === 'pending' && friend.friendRequest === 'them') {
+        //   return (
+        //     <div>
+        //       <Button className='btn btn-danger' id={friend.friendRequestId} onClick={this.deleteFriendButton}>X</Button>
+        //     </div>
+        //   );
+      } if (status === 'potentials') {
+        return (
+          <Button className='btn btn-success' id={friend.uid} onClick={this.makeNewFriend}>+</Button>
+        );
       }
       return '';
     };
